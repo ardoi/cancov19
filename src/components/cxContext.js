b8d833604a01a7ea2a34cb3a3e26b4f2007ee014
Ardo Illaste <ardo.illaste@gmail.com>
2020-03-30 23:49:53 -0600

    More robust data fetching. If google fails try another source

--- src/components/cxContext.js
+++ src/components/cxContext.js
@@ -20,44 +20,78 @@ export default class DataContext extends Component {
             return;
         }
         const dateFormat = "D-MMM-YYYY";
-        const url =
-            "https://docs.google.com/spreadsheets/d/1C59nxtgcnwGyo6lgypsgN18duxmwWigjeVdKY58t0mU/gviz/tq?tqx=out:csv&sheet=Detail";
+        const urlBase =
+            "https://docs.google.com/spreadsheets/d/1C59nxtgcnwGyo6lgypsgN18duxmwWigjeVdKY58t0mU/gviz/tq?tqx=out:csv&sheet=";
+        const url = urlBase + "DT&range=C:G";
+        const url2g = urlBase + "Dth&range=B:F";
         const url2 =
-            "https://docs.google.com/spreadsheets/d/1C59nxtgcnwGyo6lgypsgN18duxmwWigjeVdKY58t0mU/gviz/tq?tqx=out:csv&sheet=Dth";
+            "https://docs.googe.com/spreadsheets/d/1C59nxtgcnwGyo6lgypsgN18duxmwWigjeVdKY58t0mU/gviz/tq?tqx=out:csv&sheet=";
         this.setState({ loading: true });
-        csv(url)
-            .then(data => {
-                data.forEach(d => {
-                    d.Date = moment(d.Date + "-2020", dateFormat);
-                });
-                data = data.filter(x => x.Prov !== "RC");
-                this.cf = crossfilter(data);
-                const clonedData = JSON.parse(JSON.stringify(data));
-                clonedData.forEach(x => {
-                    x.Date = moment(x.Date);
-                });
-                this.cfn = crossfilter(clonedData);
-                this.setState({ detailData: data });
-            })
-            .then(() =>
-                csv(url2).then(data => {
-                    data.forEach(d => {
-                        d.Date = moment(d["Announced\n"] + "-2020", dateFormat);
+
+        const processDetailData = data => {
+            data.forEach(d => {
+                d.Date = moment(d.Date + "-2020", dateFormat);
+            });
+            data = data.filter(x => x.Prov !== "RC");
+            this.cf = crossfilter(data);
+            const clonedData = JSON.parse(JSON.stringify(data));
+            clonedData.forEach(x => {
+                x.Date = moment(x.Date);
+            });
+            this.cfn = crossfilter(clonedData);
+            return data;
+        };
+
+        const processDeathData = data => {
+            console.log(data);
+            data.forEach(d => {
+                d.Date = moment(d["Announced\n"] + "-2020", dateFormat);
+            });
+            data = data.filter(x => x.Prov !== "RC");
+            this.cfd = crossfilter(data);
+            const clonedData = JSON.parse(JSON.stringify(data));
+            clonedData.forEach(x => {
+                x.Date = moment(x.Date);
+            });
+            this.cfdn = crossfilter(clonedData);
+            return data;
+        };
+        //backup URL's in case there are CORS errors
+        const backupUrlBase = "https://cors-anywhere.herokuapp.com/";
+        const backupUrl = backupUrlBase + url;
+        const backupUrl2 = backupUrlBase + url2g;
+
+        const p1 = csv(url)
+            .then(processDetailData)
+            .catch(error => {
+                console.log("failed with url", error);
+                return csv(backupUrl)
+                    .then(processDeathData)
+                    .catch(error => {
+                        console.log("backuperror 1", error);
                     });
-                    data = data.filter(x => x.Prov !== "RC");
-                    this.cfd = crossfilter(data);
-                    const clonedData = JSON.parse(JSON.stringify(data));
-                    clonedData.forEach(x => {
-                        x.Date = moment(x.Date);
+            });
+
+        const p2 = csv(url2g)
+            .then(processDeathData)
+            .catch(error => {
+                console.log("failed with url2", error);
+                return csv(backupUrl2)
+                    .then(processDeathData)
+                    .catch(error => {
+                        console.log("backuperror 2", error);
                     });
-                    this.cfdn = crossfilter(clonedData);
-                    this.setState({
-                        loading: false,
-                        hasCF: true,
-                        deathData: data
-                    });
-                })
-            );
+            });
+
+        Promise.all([p1, p2]).then(x => {
+            console.log("all done", x);
+            this.setState({
+                loading: false,
+                hasCF: true,
+                detailData: x[0],
+                deathData: x[1]
+            });
+        });
     }
     render() {
         if (!this.state.hasCF) {
